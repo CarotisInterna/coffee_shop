29352346282fec0d46680eaea37624a98c86f845
popova <popova@reksoft.ru>
2019-01-18 15:13:57 +0300

    add psvm and mvc configs

---
+++ src/main/java/ru/popova/practice/shop/config/SpringWebConfig.java
@@ -0,0 +1,15 @@
+package ru.popova.practice.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class SpringWebConfig implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
