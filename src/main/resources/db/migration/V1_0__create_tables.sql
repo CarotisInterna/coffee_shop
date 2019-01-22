affe3a221945c662741b0ca93159df6a2def130b
CarotisInterna <interna.carotis@gmail.com>
2019-01-22 12:53:17 +0300

    delete on delete cascase

--- src/main/resources/db/migration/V1_0__create_tables.sql
+++ src/main/resources/db/migration/V1_0__create_tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS app_user (
   password VARCHAR(255) NOT NULL,
   phone_number VARCHAR(11) NOT NULL UNIQUE,
   address TEXT NOT NULL,
-  role_id INT REFERENCES role (id) ON DELETE CASCADE
+  role_id INT REFERENCES role (id)
 );
 
 --create table drink category
@@ -33,15 +33,15 @@ CREATE TABLE IF NOT EXISTS drink (
 
 --create table with drink category
 CREATE TABLE IF NOT EXISTS drink_category (
-  drink_id INT NOT NULL REFERENCES drink (id) ON DELETE CASCADE,
-  category_id INT NOT NULL REFERENCES category (id) ON DELETE CASCADE
+  drink_id INT NOT NULL REFERENCES drink (id),
+  category_id INT NOT NULL REFERENCES category (id)
 );
 
 --create table with drink image
 CREATE TABLE IF NOT EXISTS drink_img (
   id SERIAL NOT NULL PRIMARY KEY,
   image TEXT NOT NULL,
-  drink_id INT NOT NULL REFERENCES drink (id) ON DELETE CASCADE
+  drink_id INT NOT NULL REFERENCES drink (id)
 );
 
 --create table with toppings
@@ -61,8 +61,8 @@ CREATE TABLE IF NOT EXISTS order_status (
 --create table with orders
 CREATE TABLE IF NOT EXISTS orders (
   id SERIAL NOT NULL PRIMARY KEY,
-  status_id INT NOT NULL REFERENCES order_status (id) ON DELETE CASCADE,
-  customer_id INT NOT NULL REFERENCES app_user (id) ON DELETE CASCADE,
+  status_id INT NOT NULL REFERENCES order_status (id),
+  customer_id INT NOT NULL REFERENCES app_user (id),
   total REAL NOT NULL CHECK (total > 0),
   address TEXT NOT NULL,
   date TIMESTAMP NOT NULL
@@ -71,15 +71,15 @@ CREATE TABLE IF NOT EXISTS orders (
 --create table with ordered drinks
 CREATE TABLE IF NOT EXISTS drink_order (
   id SERIAL NOT NULL PRIMARY KEY,
-  order_id INT NOT NULL REFERENCES orders (id) ON DELETE CASCADE,
-  drink_id INT NOT NULL REFERENCES drink (id) ON DELETE CASCADE,
+  order_id INT NOT NULL REFERENCES orders (id),
+  drink_id INT NOT NULL REFERENCES drink (id),
   quantity SMALLINT NOT NULL CHECK (quantity > 0 AND quantity < 51)
 );
 
 --create table with customer's drink configs
 CREATE TABLE IF NOT EXISTS topping_for_drink_in_order (
-  drink_order_id INT NOT NULL REFERENCES drink_order (id) ON DELETE CASCADE,
-  topping_id INT NOT NULL REFERENCES topping (id) ON DELETE CASCADE,
+  drink_order_id INT NOT NULL REFERENCES drink_order (id),
+  topping_id INT NOT NULL REFERENCES topping (id),
   quantity SMALLINT NOT NULL CHECK (quantity > 0 AND quantity < 4),
   CONSTRAINT uc_topping_in_drink UNIQUE (drink_order_id, topping_id)
 );
