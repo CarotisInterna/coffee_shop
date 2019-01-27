168a0c176ba3a13b79f9e17c2aacdee528490142
CarotisInterna <interna.carotis@gmail.com>
2019-01-28 00:35:26 +0300

    джавадок

--- src/main/java/ru/popova/practice/shop/service/AbstractMapper.java
+++ src/main/java/ru/popova/practice/shop/service/AbstractMapper.java
@@ -1,5 +1,10 @@
 package ru.popova.practice.shop.service;
 
 public interface AbstractMapper<E, D> {
+    /**
+     * маппинг сущности в дто
+     * @param entity сущность
+     * @return дто
+     */
     D toDto(E entity);
 }
