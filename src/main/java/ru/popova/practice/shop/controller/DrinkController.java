40254f02a9c51fecbdd25620ed8712108788c514
popova <popova@reksoft.ru>
2019-01-28 16:34:31 +0300

    получение напитка по id, получение напитков конкретной категории

--- src/main/java/ru/popova/practice/shop/controller/DrinkController.java
+++ src/main/java/ru/popova/practice/shop/controller/DrinkController.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.popova.practice.shop.dto.DrinkDto;
 import ru.popova.practice.shop.service.DrinkService;
 
@@ -16,15 +13,45 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/drinks")
+@RequestMapping("/api/drinks")
 public class DrinkController {
 
     private DrinkService drinkService;
 
-    @GetMapping("/")
-    public List<DrinkDto> getDrinks(@PageableDefault Pageable pageable) {
+    /**
+     * получение списка напитков
+     * @param pageable
+     * @return список напитков
+     */
+    @GetMapping
+    public ResponseEntity<List<DrinkDto>> getDrinks(@PageableDefault Pageable pageable) {
         Page<DrinkDto> drinks = drinkService.getDrinks(pageable);
-        return drinks.getContent();
+        return ResponseEntity.ok(drinks.getContent());
+    }
+
+    /**
+     * получение списка напитков конкретной категории
+     * @param categoryId идентификатор категории
+     * @return список напитков заданной категории
+     */
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<List<DrinkDto>> getDrinksByCategory(@PathVariable Integer categoryId) {
+        List<DrinkDto> drinks = drinkService.getDrinksByCategory(categoryId);
+        return ResponseEntity.ok(drinks);
+    }
+
+    /**
+     * получение напитка по id
+     * @param id идентификатор напитка
+     * @return напиток или 404
+     */
+    @GetMapping("/{categoryId}/{id}")
+    public ResponseEntity<DrinkDto> getDrinkById(@PathVariable Integer id) {
+        return drinkService.getDrinkById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity
+                        .notFound()
+                        .build());
     }
 
     @Autowired
