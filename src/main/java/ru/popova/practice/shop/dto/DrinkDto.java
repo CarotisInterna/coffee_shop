36942936481ba393bf3842dc15e76155efa46525
popova <popova@reksoft.ru>
2019-01-28 16:31:50 +0300

    добавлен id и список картинок

--- src/main/java/ru/popova/practice/shop/dto/DrinkDto.java
+++ src/main/java/ru/popova/practice/shop/dto/DrinkDto.java
@@ -6,13 +6,16 @@ import lombok.Setter;
 import ru.popova.practice.shop.entity.DrinkEntity;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class DrinkDto {
+    private Integer id;
     private String name;
     private BigDecimal price;
     private Integer volume;
+    private List<String> images;
     private String description;
 }
