9e7c80949cc1ed52d6219e5c6b5341f1581f22c0
popova <popova@reksoft.ru>
2019-01-30 16:49:58 +0300

    джава док

--- src/main/java/ru/popova/practice/shop/specification/SpecificationBuilder.java
+++ src/main/java/ru/popova/practice/shop/specification/SpecificationBuilder.java
@@ -6,19 +6,38 @@ import javax.persistence.metamodel.SingularAttribute;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * сборщик спецификаций для поиска напитков по заданным параметрам
+ *
+ * @param <T> для какого типа объектов создается спецификация
+ */
 public class SpecificationBuilder<T> {
 
+    /**
+     * список спецификаций
+     */
     private List<Specification<T>> specifications;
 
     public SpecificationBuilder() {
         specifications = new ArrayList<>();
     }
 
+    /**
+     * добавление спецификации
+     *
+     * @param specification спецификация
+     * @return
+     */
     public SpecificationBuilder with(Specification<T> specification) {
         specifications.add(specification);
         return this;
     }
 
+    /**
+     * сборка спецификации из нескольких спецификаций
+     *
+     * @return спецификация
+     */
     public Specification<T> build() {
         if (specifications.isEmpty()) {
             return null;
@@ -32,6 +51,15 @@ public class SpecificationBuilder<T> {
         return result;
     }
 
+    /**
+     * создание объекта для фильтрации с помощью операций сравнения
+     *
+     * @param name  название поля, для которого применяется фильтрация
+     * @param lower нижняя граница
+     * @param upper верхняя граница
+     * @param <X>   тип границ
+     * @return объект с необходимой спецификацией
+     */
     public <X extends Comparable<X>> SpecificationBuilder<T> between(SingularAttribute<T, X> name, X lower, X upper) {
         if (lower != null && upper != null) {
             this.with(((root, query, builder) ->
