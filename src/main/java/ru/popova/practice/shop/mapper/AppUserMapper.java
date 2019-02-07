ff0adb68ab8b92731f6b809d1b3578cea90549d8
popova <popova@reksoft.ru>
2019-02-07 17:45:24 +0300

    маппинг пользователя, нового пользователя

---
+++ src/main/java/ru/popova/practice/shop/mapper/AppUserMapper.java
@@ -0,0 +1,28 @@
+package ru.popova.practice.shop.mapper;
+
+import ru.popova.practice.shop.dto.AppUserDto;
+import ru.popova.practice.shop.entity.AppUserEntity;
+
+public class AppUserMapper implements AbstractMapper<AppUserEntity, AppUserDto> {
+
+    @Override
+    public AppUserDto toDto(AppUserEntity appUserEntity) {
+        if (appUserEntity == null) {
+            return null;
+        }
+
+        AppUserDto appUser = new AppUserDto();
+        appUser.setUsername(appUserEntity.getUsername());
+        appUser.setPassword(appUserEntity.getPassword());
+        appUser.setAddress(appUserEntity.getAddress());
+        appUser.setFullName(appUserEntity.getFullName());
+        appUser.setPhoneNumber(appUserEntity.getPhoneNumber());
+
+        return appUser;
+    }
+
+    @Override
+    public AppUserEntity toEntity(AppUserDto dto) {
+        return null;
+    }
+}
---
+++ src/main/java/ru/popova/practice/shop/mapper/NewAppUserMapper.java
@@ -0,0 +1,44 @@
+package ru.popova.practice.shop.mapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import ru.popova.practice.shop.dto.NewAppUserDto;
+import ru.popova.practice.shop.entity.AppUserEntity;
+import ru.popova.practice.shop.entity.code.RoleCode;
+import ru.popova.practice.shop.repository.RoleEntityRepository;
+
+@Component
+public class NewAppUserMapper implements AbstractMapper<AppUserEntity, NewAppUserDto> {
+
+    private RoleEntityRepository roleEntityRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public NewAppUserMapper(RoleEntityRepository roleEntityRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.roleEntityRepository = roleEntityRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public NewAppUserDto toDto(AppUserEntity entity) {
+        return null;
+    }
+
+    @Override
+    public AppUserEntity toEntity(NewAppUserDto newAppUserDto) {
+        if(newAppUserDto == null) {
+            return null;
+        }
+
+        AppUserEntity appUser = new AppUserEntity();
+        appUser.setUsername(newAppUserDto.getUsername());
+        appUser.setPassword(bCryptPasswordEncoder.encode(newAppUserDto.getPassword()));
+        appUser.setAddress(newAppUserDto.getAddress());
+        appUser.setFullName(newAppUserDto.getFullName());
+        appUser.setRole(roleEntityRepository.findRoleEntityByCode(RoleCode.ROLE_USER.name()));
+        appUser.setPhoneNumber(newAppUserDto.getPhoneNumber());
+
+        return appUser;
+    }
+}
