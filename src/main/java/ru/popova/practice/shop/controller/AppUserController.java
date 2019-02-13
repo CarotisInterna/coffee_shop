e7780ff9afbbbb9b7b5f6261fddcbf39fff18fb6
CarotisInterna <interna.carotis@gmail.com>
2019-02-14 01:01:00 +0300

    регистрация и логин

--- src/main/java/ru/popova/practice/shop/controller/AppUserController.java
+++ src/main/java/ru/popova/practice/shop/controller/AppUserController.java
@@ -2,34 +2,64 @@ package ru.popova.practice.shop.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.popova.practice.shop.dto.AppUserDto;
+import ru.popova.practice.shop.dto.AppUserLoginDto;
 import ru.popova.practice.shop.dto.NewAppUserDto;
 import ru.popova.practice.shop.exception.ValidationException;
 import ru.popova.practice.shop.service.AppUserService;
 import ru.popova.practice.shop.service.security.SecurityService;
 
 @RestController
-@RequestMapping("/register")
 @RequiredArgsConstructor
 public class AppUserController {
 
     private final AppUserService appUserService;
+
     private final SecurityService securityService;
 
-    @PostMapping
-    public ResponseEntity<AppUserDto> register(@Validated @RequestBody NewAppUserDto newAppUserDto, BindingResult result) {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@Validated @RequestBody NewAppUserDto newAppUserDto, BindingResult result) {
         if (result.hasErrors()) {
             throw new ValidationException(result);
         }
-        AppUserDto body = appUserService.saveAppUser(newAppUserDto);
-        securityService.authenticateUser(body);
-        return ResponseEntity.ok(body);
+
+        appUserService.saveAppUser(newAppUserDto);
+
+        securityService.authenticateUser(newAppUserDto);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody AppUserLoginDto appUserLoginDto, BindingResult result) {
+        if (result.hasErrors()) {
+            throw new ValidationException(result);
+        }
+
+        String name = appUserLoginDto.getUsername();
+        AppUserDto appUserByUsername = appUserService.getAppUserByUsername(name);
+        if (appUserByUsername == null) {
+            throw new UsernameNotFoundException("такого пользователя нет");
+        }
+
+        String password = appUserLoginDto.getPassword();
+        if (!bCryptPasswordEncoder.matches(password, appUserByUsername.getPassword())) {
+            throw  new BadCredentialsException("неверный пароль");
+        }
+
+        securityService.authenticateUser(appUserLoginDto);
+
+        return ResponseEntity.ok().build();
     }
 }
 
