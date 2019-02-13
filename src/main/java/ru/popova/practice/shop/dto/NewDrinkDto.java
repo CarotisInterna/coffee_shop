16cf3166730edf1b5cda3398024e5a95c3b2a329
popova <popova@reksoft.ru>
2019-02-13 12:40:17 +0300

    убраны валидационные сообщения

--- src/main/java/ru/popova/practice/shop/dto/NewDrinkDto.java
+++ src/main/java/ru/popova/practice/shop/dto/NewDrinkDto.java
@@ -12,32 +12,32 @@ import java.util.List;
 @Setter
 @Getter
 @NoArgsConstructor
-@NameAndVolumeUnique(message = "Напиток с таким наименованием в таком объеме уже существует")
+@NameAndVolumeUnique
 public class NewDrinkDto {
 
     @NotBlank//(message = "Наименование не может быть пустым или пробелом")
-    @Size(min = 3, max = 20, message = "Наименование должно быть от 3 до 20 символов")
+    @Size(min = 3, max = 20)
     private String name;
 
     @NotNull//(message = "Цена не может быть пустой")
-    @Min(value = 30, message = "Цена не может быть меньше 30")
-    @Max(value = 1000, message = "Цена не может быть больше 1000")
+    @Min(value = 30)
+    @Max(value = 1000)
     private BigDecimal price;
 
     @NotNull//(message = "Объем не может быть пустым")
-    @Min(value = 100, message = "Объем не может быть меньше 100")
-    @Max(value = 1000, message = "Объем не может быть больше 1000")
+    @Min(value = 100)
+    @Max(value = 1000)
     private Integer volume;
 
     @NotEmpty//(message = "Список картинок не может быть пустым")
-    @Size(min = 1, max = 3, message = "Картинок может быть от 1 до 3")
+    @Size(min = 1, max = 3)
     private List<@NotBlank String> images;
 
     @NotBlank//(message = "Описание не может быть пустым или пробелом")
-    @Size(min = 10, max = 300, message = "Описание должно быть от 3 до 20 символов")
+    @Size(min = 10, max = 300)
     private String description;
 
     @NotEmpty//(message = "Список категорий не может быть пустым")
-    @Size(min = 1, max = 5, message = "Категорий может быть от 1 до 5")
+    @Size(min = 1, max = 5)
     private List<@NotNull Integer> categories;
 }
