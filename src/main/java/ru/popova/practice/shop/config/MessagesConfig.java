10965da0b6df554eb3e1416710fff857b28d8bd3
CarotisInterna <interna.carotis@gmail.com>
2019-02-13 09:07:17 +0300

    попытка настройки локалей

--- src/main/java/ru/popova/practice/shop/config/MessagesConfig.java
+++ src/main/java/ru/popova/practice/shop/config/MessagesConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 
 @Configuration
@@ -23,5 +27,12 @@ public class MessagesConfig {
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(messageSource());
         return bean;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
     }
 }
