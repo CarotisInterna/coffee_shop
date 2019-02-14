58b32389387e507920937e0cc110178700600f99
popova <popova@reksoft.ru>
2019-02-14 17:46:39 +0300

    исправлен доступ к сваггеру без логина, изменена версия

--- src/main/java/ru/popova/practice/shop/config/SpringSecurityConfig.java
+++ src/main/java/ru/popova/practice/shop/config/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,7 +22,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/v2/api-docs", "/register", "/login")
+                .antMatchers("/", "/v2/api-docs", "/register", "/login", "/api/drinks", "/swagger-ui.html")
                 .permitAll()
                 .antMatchers("/api/categories").hasRole("VENDOR")
                 .antMatchers(HttpMethod.POST, "/api/drinks").hasRole("VENDOR")
@@ -35,6 +36,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().
+                antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html/**", "/webjars/**", "/resources/**", "/swagger-resources/configuration/ui");
     }
 
     @Bean
