67a58c5fd52cce72d470683a25153b21ef0125c6
popova <popova@reksoft.ru>
2019-02-25 17:21:02 +0300

    удаление, получение корзины текущего пользователя, подсчет цены

--- src/main/java/ru/popova/practice/shop/controller/CartController.java
+++ src/main/java/ru/popova/practice/shop/controller/CartController.java
@@ -2,10 +2,7 @@ package ru.popova.practice.shop.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.popova.practice.shop.dto.DrinkOrderDto;
 import ru.popova.practice.shop.dto.OrderDto;
 import ru.popova.practice.shop.service.CartService;
@@ -19,12 +16,35 @@ public class CartController {
 
     /**
      * Добавление напитка в корзину
+     *
      * @param drinkOrderDto напиток в заказе
-     * @return сохраненный заказ
+     * @return статус
      */
     @PostMapping
     public ResponseEntity<OrderDto> addToCart(@RequestBody DrinkOrderDto drinkOrderDto) {
         OrderDto saved = cartService.addProductToCart(drinkOrderDto);
         return ResponseEntity.ok(saved);
+    }
+
+    /**
+     * Получение корзины текущего пользователя
+     *
+     * @return статус
+     */
+    @GetMapping
+    public ResponseEntity<OrderDto> getCurrentUserCart() {
+        return ResponseEntity.ok(cartService.getCurrentUserCart());
+    }
+
+    /**
+     * Удаление напитка из корзины
+     *
+     * @param id идентификатор напитка в заказе
+     * @return статус
+     */
+    @DeleteMapping
+    public ResponseEntity deleteDrinkFromCart(@RequestBody Integer id) {
+        cartService.deleteProductFromCart(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/ru/popova/practice/shop/service/CartService.java
+++ src/main/java/ru/popova/practice/shop/service/CartService.java
@@ -3,20 +3,22 @@ package ru.popova.practice.shop.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.popova.practice.shop.config.messages.MessageSourceDecorator;
 import ru.popova.practice.shop.dto.DrinkOrderDto;
 import ru.popova.practice.shop.dto.OrderDto;
-import ru.popova.practice.shop.entity.AppUserEntity;
-import ru.popova.practice.shop.entity.OrderEntity;
-import ru.popova.practice.shop.entity.OrderStatusEntity;
+import ru.popova.practice.shop.entity.*;
 import ru.popova.practice.shop.entity.code.OrderStatusCode;
+import ru.popova.practice.shop.exception.InvalidOperationException;
 import ru.popova.practice.shop.mapper.AppUserMapper;
 import ru.popova.practice.shop.mapper.DrinkOrderMapper;
 import ru.popova.practice.shop.mapper.OrderMapper;
+import ru.popova.practice.shop.repository.DrinkOrderEntityRepository;
 import ru.popova.practice.shop.repository.OrderEntityRepository;
 import ru.popova.practice.shop.repository.OrderStatusEntityRepository;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,11 +26,12 @@ import java.util.Optional;
 public class CartService {
 
     private final AppUserService appUserService;
-    private final AppUserMapper appUserMapper;
     private final OrderMapper orderMapper;
+    private final DrinkOrderMapper drinkOrderMapper;
     private final OrderEntityRepository orderEntityRepository;
     private final OrderStatusEntityRepository orderStatusEntityRepository;
-    private final DrinkOrderMapper drinkOrderMapper;
+    private final DrinkOrderEntityRepository drinkOrderEntityRepository;
+    private final MessageSourceDecorator message;
 
     /**
      * Получение корзины текущего пользователя
@@ -41,12 +44,30 @@ public class CartService {
         return orderMapper.toDto(cart);
     }
 
+    /**
+     * Добавление напитка в корзину
+     *
+     * @param drinkOrderDto дто напитка в заказе
+     * @return дто сохраненного напитка в заказе
+     */
     @Transactional
     public OrderDto addProductToCart(DrinkOrderDto drinkOrderDto) {
         OrderEntity cart = getCurrentUserCartEntity().orElse(getEmptyCartEntity());
         cart.addDrink(drinkOrderMapper.toEntity(drinkOrderDto));
+        setTotalInOrder(cart);
         OrderEntity saved = orderEntityRepository.save(cart);
         return orderMapper.toDto(saved);
+    }
+
+    /**
+     * Удаление напитка из корзины
+     *
+     * @param drinkOrderId идентификатор напитка в заказе
+     */
+    @Transactional
+    public void deleteProductFromCart(Integer drinkOrderId) {
+        OrderEntity cart = getCurrentUserCartEntity().orElseThrow(() -> new InvalidOperationException("cart", message.getMessage("CartIsEmpty.message")));
+        drinkOrderEntityRepository.findById(drinkOrderId).ifPresent(drinkOrderEntityRepository::delete);
     }
 
     /**
@@ -71,9 +92,40 @@ public class CartService {
      *
      * @return сущность сыночки-корзиночки текущего пользователя
      */
-    public Optional<OrderEntity> getCurrentUserCartEntity() {
+    private Optional<OrderEntity> getCurrentUserCartEntity() {
         Integer userId = appUserService.getCurrentUser().getId();
         return orderEntityRepository.findOneByAppUserIdAndOrderStatusCode(userId, OrderStatusCode.IN_CART);
+    }
+
+    /**
+     * Подсчет итога корзины
+     *
+     * @param order сущность заказа
+     */
+    @Transactional
+    public void setTotalInOrder(OrderEntity order) {
+        List<DrinkOrderEntity> drinks = order.getDrinks();
+        BigDecimal total = BigDecimal.ZERO;
+
+        for (DrinkOrderEntity drink : drinks) {
+
+            DrinkEntity drinkEntity = drink.getDrink();
+            BigDecimal drinkQuantity = new BigDecimal(drink.getQuantity());
+            total = total.add(drinkEntity.getPrice()
+                    .multiply(drinkQuantity));
+            List<ToppingForDrinkInOrderEntity> toppings = drink.getToppings();
+
+            for (ToppingForDrinkInOrderEntity topping : toppings) {
+
+                ToppingEntity toppingEntity = topping.getTopping();
+                BigDecimal toppingQuantity = new BigDecimal(topping.getQuantity());
+                total = total.add(toppingEntity.getPrice()
+                        .multiply(toppingQuantity));
+
+            }
+        }
+
+        order.setTotal(total);
     }
 
 }
