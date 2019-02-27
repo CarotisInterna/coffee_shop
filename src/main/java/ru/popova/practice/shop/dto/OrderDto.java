a862036d543c7dca5b85b96edffbd245ce78d540
popova <popova@reksoft.ru>
2019-02-27 17:56:59 +0300

    валидация заказа, новые сообщения, новая валидационная группа

--- src/main/java/ru/popova/practice/shop/dto/OrderDto.java
+++ src/main/java/ru/popova/practice/shop/dto/OrderDto.java
@@ -3,7 +3,12 @@ package ru.popova.practice.shop.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.popova.practice.shop.dto.groups.PlaceOrderGroup;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,7 +22,14 @@ public class OrderDto {
     private String orderStatus;
     private String appUser;
     private BigDecimal total;
+
+    @NotBlank(groups = PlaceOrderGroup.class)
+    @Min(value = 10)
+    @Max(value = 100)
     private String address;
+
     private LocalDateTime date;
+
+    @NotEmpty(groups = PlaceOrderGroup.class)
     private List<DrinkOrderDto> drinks;
 }
---
+++ src/main/java/ru/popova/practice/shop/dto/groups/PlaceOrderGroup.java
@@ -0,0 +1,7 @@
+package ru.popova.practice.shop.dto.groups;
+
+/**
+ * Интерфейс для валидации полей при размещении заказа
+ */
+public interface PlaceOrderGroup {
+}
