57266c40f8121a88a6fb9acc3237192208519b6e
popova <popova@reksoft.ru>
2019-03-26 16:13:17 +0300

    новые тесты контроллера

--- src/test/java/ru/popova/practice/shop/DrinkControllerTest.java
+++ src/test/java/ru/popova/practice/shop/DrinkControllerTest.java
@@ -11,16 +11,17 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
 import ru.popova.practice.shop.dto.NewDrinkDto;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -118,26 +119,150 @@ public class DrinkControllerTest {
     }
 
     /**
+     * Тест сохранения уже существующего напитка
+     *
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser(roles = "VENDOR")
+    public void saveAlreadyExistsDrink() throws Exception {
+
+        NewDrinkDto americano = createNewDrink("Американо",
+                150,
+                150,
+                "Десертный напиток на основе молока и мороженого.",
+                2, 5
+        );
+
+        this.mockMvc
+                .perform(post("/api/drinks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(americano)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorDtos[0].message").value("Напиток с таким наименованием в таком объеме уже существует"));
+    }
+
+    /**
+     * Тест валидационных сообщений
+     *
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser(roles = "VENDOR")
+    public void testDrinkValidation() throws Exception {
+
+        /*
+        валидация имени
+         */
+        NewDrinkDto drink = createNewDrink("А",
+                500,
+                100,
+                "Десеhjhjhjh",
+                2
+        );
+
+        this.mockMvc
+                .perform(post("/api/drinks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(drink)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorDtos[0].message").value("Может быть от 3 до 20"));
+
+        /*
+        валидация цены
+         */
+        drink.setName("Новый напиток");
+        drink.setPrice(new BigDecimal(5005));
+        this.mockMvc
+                .perform(post("/api/drinks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(drink)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorDtos[0].message").value("Не может быть больше 1000"));
+
+    }
+
+    /**
+     * Тест попытки сохранения несуществующей категории
+     *
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser(roles = "VENDOR")
+    public void testCategoryDoesNotExist() throws Exception {
+
+        NewDrinkDto drink = createNewDrink("Новый напиток",
+                500,
+                100,
+                "Десеhjhjhjh",
+                2, 100
+        );
+
+        this.mockMvc
+                .perform(post("/api/drinks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(drink)))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errorDtos[0].message").value("100 Категория не найдена"));
+
+    }
+
+    /**
+     * Тест редактирования не существующего напитка
+     *
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser(roles = "VENDOR")
+    public void testEditNotExistedDrink() throws Exception {
+
+        NewDrinkDto drink = createBananaCocktail();
+
+        this.mockMvc
+                .perform(put("/api/drinks/100")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(drink)))
+                .andExpect(status().isNotFound());
+    }
+
+    /**
      * Создание корректного дто напитка для добавления
      *
-     * @return
+     * @return дто нового напитка
      */
     public NewDrinkDto createBananaCocktail() {
 
-        List<Integer> categories = new ArrayList<>();
-        categories.add(2);
-        categories.add(5);
+        return createNewDrink("Банановый коктейль",
+                150,
+                300,
+                "Десертный напиток на основе молока и мороженого.",
+                2, 5
+        );
+    }
 
-        List<String> images = new ArrayList<>();
-        images.add("banana_cocktail");
+    /**
+     * Создание напитка
+     *
+     * @param name        наименование
+     * @param price       цена
+     * @param volume      объем
+     * @param description описание
+     * @param categories  категории напитка
+     * @return дто нового напитка
+     */
+    public NewDrinkDto createNewDrink(String name, Integer price, Integer volume, String description, Integer... categories) {
+
+        List<Integer> categoriesList = new ArrayList<>(Arrays.asList(categories));
 
         return new NewDrinkDto(
-                "Банановый коктейль",
-                new BigDecimal(150),
-                300,
-                Collections.emptyList(),
-                "Десертный напиток на основе молока и мороженого.",
-                categories
+                name,
+                new BigDecimal(price),
+                volume, Collections.emptyList(),
+                description,
+                categoriesList
         );
     }
 }
