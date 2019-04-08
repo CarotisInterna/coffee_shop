49a08067e3bb664534ab09df90bf895e6159623d
popova <popova@reksoft.ru>
2019-04-08 18:12:51 +0300

    новый профиль, проперти для разных профилей

---
+++ src/main/resources/db/migration/docker/V1_3__test_toppings.sql
@@ -0,0 +1,13 @@
+--заполнение топпингов
+INSERT INTO topping(name, price) VALUES('Сахар', 0);
+INSERT INTO topping(name, price) VALUES('Сливки', 10);
+INSERT INTO topping(name, price) VALUES('Взбитые сливки', 15);
+INSERT INTO topping(name, price) VALUES('Мармеладные мишки', 20);
+INSERT INTO topping(name, price) VALUES('Сироп клубничный', 10);
+INSERT INTO topping(name, price) VALUES('Сироп шоколадный', 10);
+INSERT INTO topping(name, price) VALUES('Сироп Апельсиновый', 10);
+INSERT INTO topping(name, price) VALUES('Сироп белый шоколад', 10);
+INSERT INTO topping(name, price) VALUES('Сироп малиновый', 10);
+INSERT INTO topping(name, price) VALUES('Карамель', 15);
+INSERT INTO topping(name, price) VALUES('Маршмеллоу', 20);
+INSERT INTO topping(name, price) VALUES('Печенье', 15);
